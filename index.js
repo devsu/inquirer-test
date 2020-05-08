9bb679919d1e1c410f62d54b82621a8bcfdf54f0
Cesar Salazar <csalazar@devsu.com>
2020-05-07 21:22:39 -0500

    change timeout options

--- index.js
+++ index.js
@@ -7,26 +7,29 @@ const ENTER = '\x0D';
 
 module.exports = async function(args, combo, options) {
   const defaultOptions = {
-    timeout: 5,
-    initialTimeout: 200,
-    enterTimeout: 10,
+    waitBeforeKeystroke: 50,
+    waitBeforeStart: 200,
+    waitAfterDone: 200,
   };
   options = Object.assign({}, defaultOptions, options);
-  let nextTimeout = options.initialTimeout;
 
   var proc = spawn('node', args, { stdio: [null, null, null] });
   proc.stdin.setEncoding('utf-8');
 
-  var loop = function(combo) {
-    if (combo.length > 0) {
-      setTimeout(function() {
+  const len = combo.length;
+
+  const loop = (combo, i) => {
+    let wait = options.waitBeforeKeystroke;
+    if (i === 0) wait = options.waitBeforeStart;
+    if (i === len) wait = options.waitAfterDone;
+    setTimeout(() => {
+      if (combo.length > 0) {
         proc.stdin.write(combo[0]);
-        nextTimeout = combo[0] === ENTER ? options.enterTimeout : options.timeout;
-        loop(combo.slice(1));
-      }, nextTimeout);
-    } else {
-      proc.stdin.end();
-    }
+        loop(combo.slice(1), i + 1);
+      } else {
+        proc.stdin.end();
+      }
+    }, wait);
   };
 
   loop(combo);
